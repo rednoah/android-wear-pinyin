afc1c5f53e2f3e9b17c57d77f1ea07ec1fa69eb9
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-29 13:51:31 +0800

    Prepare user study harness app

--- corpus/select-sample-sentences.groovy
+++ corpus/select-sample-sentences.groovy
@@ -5,11 +5,18 @@ def xml = new XmlSlurper().parse('smsCorpus_zh_2015.03.09.xml' as File)
 def CITY_PATTERN = ~/北京/
 def TEXT_PATTERN = ~/[\p{script=Han}。，？]{3,15}/
 
+def lines = []
+
 xml.message.each{ m ->
 	if (m.source.userProfile.city ==~ CITY_PATTERN && m.source.userProfile.nativeSpeaker == 'yes') {
 		if (m.text ==~ TEXT_PATTERN) {
-			def line = [m.'@id', m.text].join('\t')
-			println line			
+			lines += m.text		
 		}
 	}
 }
+
+
+(1..10).each{ Collections.shuffle(lines) }
+
+
+lines.take(100).each{ println it }
