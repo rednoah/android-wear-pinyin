842d61c05dc56fabbcc394d567fe9ed46de1a00a
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-06-29 13:50:52 +0800

    Prepare user study harness app

---
+++ prompter/run-pilot-study.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+USER=$(expr $(cat pilot-study-participants.txt | wc -l) + 1)
+echo "USER: $USER"
+
+read -e -p "NAME: " NAME
+echo "$USER $NAME" >> pilot-study-participants.txt
+
+java -cp "bin:lib/*" ntu.csie.prompter.Prompter \
+  -Participant $USER \
+  -PhraseCount 25 \
+  -StudyPlan pilot-study-plan.tsv \
+  -PhraseSet pilot-study-phrase-set.txt \
+  -Record pilot-study-record.tsv
