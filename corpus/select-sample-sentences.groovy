63674fbb5678e9cdd443dbc05805906fdd505788
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-07-01 23:23:15 +0800

    select SMS message from Beijing users that contain between 5 and 15 hanzi

--- corpus/select-sample-sentences.groovy
+++ corpus/select-sample-sentences.groovy
@@ -2,21 +2,23 @@
 
 def xml = new XmlSlurper().parse('smsCorpus_zh_2015.03.09.xml' as File)
 
+// select SMS message from Beijing users that contain between 5 and 15 hanzi
 def CITY_PATTERN = ~/北京/
-def TEXT_PATTERN = ~/[\p{script=Han}。，？]{3,15}/
+def TEXT_PATTERN = ~/\p{script=Han}{5,15}\p{P}*/
 
 def lines = []
 
 xml.message.each{ m ->
 	if (m.source.userProfile.city ==~ CITY_PATTERN && m.source.userProfile.nativeSpeaker == 'yes') {
 		if (m.text ==~ TEXT_PATTERN) {
-			lines += m.text		
+			lines += m.text as String
 		}
 	}
 }
 
 
-(1..10).each{ Collections.shuffle(lines) }
+def list = lines.collect{ it.replaceAll(/\p{P}/, '') }.toSet().toList()
+(1..10).each{ Collections.shuffle(list, new java.security.SecureRandom()) }
 
 
-lines.take(100).each{ println it }
+list.take(75).each{ println it }
