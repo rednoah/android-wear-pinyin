81313b8bac55834ad62df2a1e817ebd02e9007e4
Reinhard Pointner <reinhard.pointner@gmail.com>
2017-07-01 12:48:29 +0800

    The correct Pinyin for "üe" is "ue" (e.g. nue'dai)

--- watch/app/src/main/java/ntu/csie/swipy/model/Initial.java
+++ watch/app/src/main/java/ntu/csie/swipy/model/Initial.java
@@ -16,8 +16,8 @@ public enum Initial {
     F("ㄈ", a, o, ei, ou, an, en, ang, eng, u),
     D("ㄉ", a, e, ai, ei, ao, ou, an, en, ang, eng, ong, i, iao, ie, iu, ian, ing, u, uo, ui, uan, un),
     T("ㄊ", a, e, ai, ao, ou, an, ang, eng, ong, i, iao, ie, ian, ing, u, uo, ui, uan, un),
-    N("ㄋ", a, e, ai, ei, ao, an, en, ang, eng, ong, i, iao, ie, iu, ian, in, iang, ing, u, uo, uan, ü, üe),
-    L("ㄌ", a, e, ai, ei, ao, ou, an, ang, eng, ong, i, iao, ie, iu, ian, in, iang, ing, u, uo, uan, un, ü, üe), // ignore 倆 lia
+    N("ㄋ", a, e, ai, ei, ao, an, en, ang, eng, ong, i, iao, ie, iu, ian, in, iang, ing, u, uo, uan, ü, ue),
+    L("ㄌ", a, e, ai, ei, ao, ou, an, ang, eng, ong, i, iao, ie, iu, ian, in, iang, ing, u, uo, uan, un, ü, ue), // ignore 倆 lia
     G("ㄍ", a, e, ai, ei, ao, ou, an, en, ang, eng, ong, u, ua, uo, uai, ui, uan, un, uang),
     K("ㄎ", a, e, ai, ao, ou, an, en, ang, eng, ong, u, ua, uo, uai, ui, uan, un, uang),
     H("ㄏ", a, e, ai, ei, ao, ou, an, en, ang, eng, ong, u, ua, uo, uai, ui, uan, un, uang),
